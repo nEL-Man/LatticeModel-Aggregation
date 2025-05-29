2713f0905ebf4d405c7eadb505e8f6ab59072ee9
MANASHWIN NELLURI <95671174+nEL-Man@users.noreply.github.com>
2025-05-29 11:44:58 -0500

    Delete abeln-hb-lattice-2e567b22ecd1 directory

--- abeln-hb-lattice-2e567b22ecd1/runScripts/gc.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-cd ../output
-
-time mpirun -np 1 -output-filename ptemp.out ../source/hblattice -i ../PDBstructures/T3LT.pdb -minT 0.10 -maxT 0.10 -S 1000 -W 1000 -aa ../matrices/aa_water2.txt -indxWater 20 -lT -noSwap -ebmu 0.003 > output.out 
-
-
-cd -
-
